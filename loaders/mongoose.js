20d18f797252d2f706696d2fba29c27c55b91a34
sbmdkl <sbmdkl@gmail.com>
2021-05-03 19:32:04 +0545

    updated database

--- loaders/mongoose.js
+++ loaders/mongoose.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 module.exports = async () => {
-  mongoose
-    .connect('mongodb://localhost:27017/recruiter', {
-      useNewUrlParser: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-      useUnifiedTopology: true,
-    })
-    .then(() => {
-      console.log('mongodb connected');
-    });
+	mongoose
+		.connect(process.env.mongoURI, {
+			useNewUrlParser: true,
+			useFindAndModify: false,
+			useCreateIndex: true,
+			useUnifiedTopology: true,
+		})
+		.then(() => {
+			console.log('mongodb connected');
+		});
 };
