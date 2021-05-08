a9e44f102d6b7152dca3935cfc81a088f845341b
sbmdkl <sbmdkl@gmail.com>
2021-05-08 22:38:19 +0545

    Circular Dependency: A->B->A, break the chain to remove

--- components/commons/services/elasticPopulate.js
+++ components/commons/services/elasticPopulate.js
@@ -1,41 +1,48 @@
-module.exports = function makeElasticPopulate({ Client, FindAllUsers, FindAllJobs }) {
-	return async function populate({ httpRequest: { body } }) {
-		try {
-			const users = await FindAllUsers({ httpRequest: { query: { limit: 0 } } });
-			console.log(users.length);
-			users.forEach((user) => {
-				Client.index({
-					index: 'recruiters',
-					id: user.id.toString(),
-					type: 'users',
-					body: {
-						name: user.name,
-						email: user.email,
-						phone: user.phone,
-						country: user.country,
-						address: '',
-						title: user.title,
-						about: '',
-					},
-				});
-			});
-			const jobs = await FindAllJobs({ httpRequest: { query: { limit: 0 } } });
-			console.log(jobs.length);
-			jobs.forEach((job) => {
-				Client.index({
-					index: 'recruiters',
-					id: job.id.toString(),
-					type: 'jobs',
-					body: {
-						title: job.title,
-						location: job.location,
-						industry: job.industry,
-					},
-				});
-			});
-			return 'indexing...';
-		} catch (e) {
-			console.log(e);
-		}
-	};
+const makeFindAllUsers = require('../../users/services/findAllUsers');
+const makeFindAllJobs = require('../../jobs/services/findAllJobs');
+const User = require('../../users/User');
+const Job = require('../../jobs/Job');
+const FindAllUsers = makeFindAllUsers({ User });
+const FindAllJobs = makeFindAllJobs({ Job });
+
+module.exports = function makeElasticPopulate({ Client }) {
+  return async function populate({ httpRequest: { body } }) {
+    try {
+      const users = await FindAllUsers({ httpRequest: { query: { limit: 0 } } });
+      console.log(users.length);
+      users.forEach((user) => {
+        Client.index({
+          index: 'recruiters',
+          id: user.id.toString(),
+          type: 'users',
+          body: {
+            name: user.name,
+            email: user.email,
+            phone: user.phone,
+            country: user.country,
+            address: '',
+            title: user.title,
+            about: '',
+          },
+        });
+      });
+      const jobs = await FindAllJobs({ httpRequest: { query: { limit: 0 } } });
+      console.log(jobs.length);
+      jobs.forEach((job) => {
+        Client.index({
+          index: 'recruiters',
+          id: job.id.toString(),
+          type: 'jobs',
+          body: {
+            title: job.title,
+            location: job.location,
+            industry: job.industry,
+          },
+        });
+      });
+      return 'indexing...';
+    } catch (e) {
+      console.log(e);
+    }
+  };
 };
--- components/commons/services/index.js
+++ components/commons/services/index.js
@@ -1,6 +1,6 @@
 const Common = require('../commonsDAL');
-const { findAll: FindAllUsers } = require('../../users/services');
-const { findAll: FindAllJobs } = require('../../jobs/services');
+// const { findAll: FindAllUsers } = require('../../users/services');
+// const { findAll: FindAllJobs } = require('../../jobs/services');
 const Client = require('../../../elasticsearch/connection');
 const makeElasticInitialize = require('./elasticInitialize');
 const makeSearch = require('./search');
@@ -10,14 +10,14 @@ const makeAddJobDocument = require('./addJobDocument');
 
 const initialize = makeElasticInitialize({ Common, Client });
 const search = makeSearch({ Common, Client });
-const populate = makeElasticPopulate({ Common, Client, FindAllUsers, FindAllJobs });
+const populate = makeElasticPopulate({ Common, Client });
 const addUserDocument = makeAddUserDocument({ Common, Client });
 const addJobDocument = makeAddJobDocument({ Common, Client });
 
 module.exports = {
-	initialize,
-	search,
-	populate,
-	addUserDocument,
-	addJobDocument,
+  initialize,
+  search,
+  populate,
+  addUserDocument,
+  addJobDocument,
 };
