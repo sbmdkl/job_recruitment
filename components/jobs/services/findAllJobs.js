c15fc5af4ab9a65086121831c68d3a697fd277c9
sbmdkl <sbmdkl@gmail.com>
2021-05-10 20:55:08 +0545

    update

--- components/jobs/services/findAllJobs.js
+++ components/jobs/services/findAllJobs.js
@@ -2,8 +2,8 @@ const { allJobsDTO } = require('../dtos');
 module.exports = function makeFindAllJobs({ Job }) {
   return async function findAllJobs({ httpRequest: { query, user } }) {
     let jobs = [];
-    if (user.roles === 'user') jobs = await Job.findAll({ query });
-    else if (user.roles === 'company') jobs = await Job.findAllJobsByCompany(user.id);
+    if (user.role === 'user') jobs = await Job.findAll({ query });
+    else if (user.role === 'company') jobs = await Job.findAllJobsByCompany(user.id);
     return allJobsDTO({ jobs });
   };
 };
