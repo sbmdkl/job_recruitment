a9af5041f29ce170decc8405a2f1fe12063429c8
sbmdkl <sbmdkl@gmail.com>
2021-05-11 11:45:43 +0545

    update

--- components/appliedjobs/services/createAppliedJob.js
+++ components/appliedjobs/services/createAppliedJob.js
@@ -3,7 +3,7 @@ const { createAppliedJobDTO } = require('../dtos');
 module.exports = function makeCreateAppliedJob({ AppliedJob }) {
   return async function createAppliedJob({ httpRequest: { body, user } }) {
     let isJobApplied = await AppliedJob.findOne({ job: body.jobId, user: user.id });
-    if (isJobApplied.user) {
+    if (isJobApplied?.user) {
       throw { error: 'You have already applied for this Job' };
     }
     AppliedJob.findOne;
