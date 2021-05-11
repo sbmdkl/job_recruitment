3f131ee1d0443c7fd044505130ba6e6655ad5201
sbmdkl <sbmdkl@gmail.com>
2021-05-11 13:44:23 +0545

    updated-skills-api

--- components/skills/skillsAPI.js
+++ components/skills/skillsAPI.js
@@ -4,11 +4,11 @@ const Route = express.Router();
 const passport = require('passport');
 // @route   api/skills/
 Route.route('/')
-	.get(passport.authenticate('both', { session: false }), skillsController.findAll)
-	.post(passport.authenticate('admin', { session: false }), skillsController.create);
+  .get(skillsController.findAll)
+  .post(passport.authenticate('admin', { session: false }), skillsController.create);
 Route.route('/:id')
-	.get(passport.authenticate('both', { session: false }), skillsController.findOne)
-	.patch(passport.authenticate('admin', { session: false }), skillsController.update);
+  .get(passport.authenticate('both', { session: false }), skillsController.findOne)
+  .patch(passport.authenticate('admin', { session: false }), skillsController.update);
 // .delete(passport.authenticate('admin', { session: false }), skillsController.destroy);
 
 module.exports = Route;
